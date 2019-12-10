ecfd486b2674bfe1cb251c40bd3439262bef18bc
Josue Carrasco <jos247car@gmail.com>
2019-12-10 16:44:57 -0300

    agregue logica de juego , y dos botones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const palabra = document.getElementById('palabra');
-const inicio = document.getElementById('inicio');
+const reinicio = document.getElementById('reinicio');
 
 //botones
 const tres = document.getElementById('tres');
@@ -15,64 +15,76 @@ const doce = document.getElementById('doce');
 const trece = document.getElementById('trece');
 const catorce = document.getElementById('catorce');
 //fin botones
-
+//siguiente palabra
+const siguiente = document.getElementById('siguientePalabra');
 //score
 const score = document.getElementById('resultado');
-var puntos = 0;
-
+let user_score = 0;
 //Arrray palabras
 const arrayPalabras = ['celebra', 'camion', 'cancion', 'helado', 'cantemos', 'cancion', 'parlante', 'computadora', 'teclado', 'celular'];
 
-//IIFE para iniciar juego
-(function jugar() {
-    inicio.addEventListener('click', function () { main() })
-})();
-
+//palabra seleccionada
+let miPalabra = '';
+//reiniciar el juego
+function reiniciarJuego() {
+    user_score = 0;
+    score.innerHTML = user_score;
+    miPalabra = '';
+}
 //elegir palabra random
 function palabraRandom() {
     let numeroRandom = Math.floor(Math.random() * 10);
-    return arrayPalabras[numeroRandom];
+    miPalabra = arrayPalabras[numeroRandom];
+    cambiarPalabra();
 }
 //cambiar palabra
-function cambiarPalabra(palabraSeleccionada) {
-    palabra.innerHTML = palabraSeleccionada;
+function cambiarPalabra() {
+    palabra.innerHTML = miPalabra;
     setTimeout(function () {
         palabra.innerHTML = '__ __ __ __';
     }, 1500);
 }
 //seleccionar boton
-function correcto(numeroSeleccionado, palabraSeleccionada) {
-    if (numeroSeleccionado === palabraSeleccionada.length) {
-        ganar();
+function correcto(numeroSeleccionado) {
+    if (miPalabra.length === 0) {
     }
     else {
-        perder();
+        if (numeroSeleccionado === miPalabra.length) {
+            ganar();
+        }
+        else {
+            perder();
+        }
     }
+
 }
 //ganar
 function ganar() {
-    puntos++;
-    score.innerHTML = puntos;
+    user_score++;
+    score.innerHTML = user_score;
 }
 //perder
 function perder() {
-    puntos--;
-    score.innerHTML = puntos;
+    user_score--;
+    score.innerHTML = user_score;
 }
+
 //funcion principal
 function main() {
-    palabraSeleccionada = palabraRandom();
-    cambiarPalabra(palabraSeleccionada);
-    tres.addEventListener('click', function () { correcto(3, palabraSeleccionada) });
-    cuatro.addEventListener('click', function () { correcto(4, palabraSeleccionada) });
-    cinco.addEventListener('click', function () { correcto(5, palabraSeleccionada) });
-    seis.addEventListener('click', function () { correcto(6, palabraSeleccionada) });
-    siete.addEventListener('click', function () { correcto(7, palabraSeleccionada) });
-    ocho.addEventListener('click', function () { correcto(8, palabraSeleccionada) });
-    nueve.addEventListener('click', function () { correcto(9, palabraSeleccionada) });
-    diez.addEventListener('click', function () { correcto(10, palabraSeleccionada) });
-    once.addEventListener('click', function () { correcto(11, palabraSeleccionada) });
-    doce.addEventListener('click', function () { correcto(12, palabraSeleccionada) });
-    trece.addEventListener('click', function () { correcto(13, palabraSeleccionada) });
-    catorce.addEventListener('click', function () { correcto(14, palabraSeleccionada) });
+    siguiente.addEventListener('click', function () { palabraRandom() });
+    tres.addEventListener('click', function () { correcto(3) });
+    cuatro.addEventListener('click', function () { correcto(4) });
+    cinco.addEventListener('click', function () { correcto(5) });
+    seis.addEventListener('click', function () { correcto(6) });
+    siete.addEventListener('click', function () { correcto(7) });
+    ocho.addEventListener('click', function () { correcto(8) });
+    nueve.addEventListener('click', function () { correcto(9) });
+    diez.addEventListener('click', function () { correcto(10) });
+    once.addEventListener('click', function () { correcto(11) });
+    doce.addEventListener('click', function () { correcto(12) });
+    trece.addEventListener('click', function () { correcto(13) });
+    catorce.addEventListener('click', function () { correcto(14) });
+    reinicio.addEventListener('click', function () { reiniciarJuego() });
 }
+
+main();
