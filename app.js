9e01c86061766ef9b12ce746f039d0249f66a3fc
Josue Carrasco <jos247car@gmail.com>
2019-12-10 18:46:13 -0300

    logica basica de juego terminada, falta ser responsive

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const palabra = document.getElementById('palabra');
 const reinicio = document.getElementById('reinicio');
-
+const resultado = document.getElementById('respuesta');
+//estado de tiempo limite
+var estado = false;
 //botones
 const tres = document.getElementById('tres');
 const cuatro = document.getElementById('cuatro');
@@ -15,54 +17,80 @@ const doce = document.getElementById('doce');
 const trece = document.getElementById('trece');
 const catorce = document.getElementById('catorce');
 //fin botones
+
 //siguiente palabra
 const siguiente = document.getElementById('siguientePalabra');
+
 //score
 const score = document.getElementById('resultado');
 let user_score = 0;
+
 //Arrray palabras
 const arrayPalabras = ['celebra', 'camion', 'cancion', 'helado', 'cantemos', 'cancion', 'parlante', 'computadora', 'teclado', 'celular'];
 
 //palabra seleccionada
 let miPalabra = '';
+
 //reiniciar el juego
 function reiniciarJuego() {
     user_score = 0;
     score.innerHTML = user_score;
     miPalabra = '';
+    cambiaResultado();
 }
+
 //elegir palabra random
 function palabraRandom() {
     let numeroRandom = Math.floor(Math.random() * 10);
     miPalabra = arrayPalabras[numeroRandom];
     cambiarPalabra();
+    cambiaResultado();
 }
-//cambiar palabra
+//cambiar palabra y resultado
 function cambiarPalabra() {
     palabra.innerHTML = miPalabra;
     setTimeout(function () {
         palabra.innerHTML = '__ __ __ __';
     }, 1500);
 }
+function cambiaResultado() {
+    resultado.innerHTML = '--';
+}
 //seleccionar boton
 function correcto(numeroSeleccionado) {
+    estado = true;
     if (miPalabra.length === 0) {
     }
     else {
         if (numeroSeleccionado === miPalabra.length) {
+            felicitacion();
             ganar();
         }
         else {
+            respuestaCorrecta(miPalabra.length);
             perder();
         }
     }
-
 }
+//muestra respuesta correcta y mensaje de correcto
+function respuestaCorrecta(respuesta) {
+    resultado.innerHTML = respuesta;
+}
+function felicitacion() {
+    resultado.innerHTML = '¡Correcta!'
+}
+
+//tiempo cumplido
+function tiempo(){
+    resultado.innerHTML = '¡Tiempo!';
+}
+
 //ganar
 function ganar() {
     user_score++;
     score.innerHTML = user_score;
 }
+
 //perder
 function perder() {
     user_score--;
@@ -71,7 +99,19 @@ function perder() {
 
 //funcion principal
 function main() {
-    siguiente.addEventListener('click', function () { palabraRandom() });
+    siguiente.addEventListener('click', function () {
+        palabraRandom();
+        setTimeout(function () {
+            if(estado){
+
+            } else if (estado === false){
+                perder();
+                miPalabra = '';
+                tiempo();
+            }                
+            },3000);
+            estado = false;
+    });
     tres.addEventListener('click', function () { correcto(3) });
     cuatro.addEventListener('click', function () { correcto(4) });
     cinco.addEventListener('click', function () { correcto(5) });
@@ -86,5 +126,4 @@ function main() {
     catorce.addEventListener('click', function () { correcto(14) });
     reinicio.addEventListener('click', function () { reiniciarJuego() });
 }
-
 main();
